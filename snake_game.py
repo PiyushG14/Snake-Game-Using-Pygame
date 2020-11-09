a30f7a9db9f73f35c7e9d4264c17e8e8af1df02e
PiyushG14 <46423477+PiyushG14@users.noreply.github.com>
2020-11-10 02:46:33 +0530

    Create snake_game.py

---
+++ snake_game.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+
+class Snake():
+    def __init__(self):
+        pass
+    
+    
+    
+class Food():
+    def __init__(self):
+        pass
+    
+def main():    
+    return
+
+    
+main()
